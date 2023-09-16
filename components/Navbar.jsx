6f78ca6a91403d5785c832366186eade69a5aa1c
Raphael Ramm <rammraphael97@gmail.com>
2023-09-16 17:53:55 +0200

    Added new Page Icon
    Not supportet in Opera GX sadly

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -25,16 +25,22 @@ const navLinks = [
 
 
 const Navbar = () => {
+  const LogoImage = "/images/Logo.png"
   const [navbarOpen, setNavbarOpen] = useState(false)
   return (
     <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
       <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
-        <Link 
-          href={"/"} 
-          className='text-2xl md:text-5xl text-white font-semibold'
-        >
-          LOGO
-        </Link>
+        <div className='flex items-center'>
+        <Link href={"/"} className='text-2xl md:text-5xl text-white font-semibold flex items-center'>
+    <Image
+      src={LogoImage}
+      alt='Logo'
+      width={50}
+      height={50}
+    />
+            <span className="ml-1">Raphael Ramm</span>
+          </Link>
+        </div>
         <div className='mobile-menu block md:hidden'>
           {
             !navbarOpen 
